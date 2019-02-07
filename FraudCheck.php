e22546a73fc7debb75588b0c5ce3d3d105427a76
Dave <davidwatson2323@gmail.com>
2019-02-07 18:46:04 +0000

    file rename

--- FraudCheck.php
+++ FraudCheck.php
@@ -8,7 +8,7 @@ class FraudDetect {
 
     public function __construct($applicationStore, $Threshold_Amount,$AplicationList) {
         $this->APPLICATION_STORE = $applicationStore;
-         $this->APPLICATION_AMOUNT_THRESHOLD = $Threshold_Amount;
+        $this->APPLICATION_AMOUNT_THRESHOLD = $Threshold_Amount;
         $this->APPLICATION_LIST = $AplicationList;    
     }
 
@@ -25,30 +25,19 @@ class FraudDetect {
 
 
     public function accumalateApplicationTransactions($applicationString){
-
-    
-      $stringArray = $this->parseString($applicationString);
+        $stringArray = $this->parseString($applicationString);
         $value =  $stringArray[2];
         $time_Stamp = $stringArray[1];
         $post_code_hash= $stringArray[0];   
 
         if( array_key_exists($post_code_hash,$this->APPLICATION_STORE)){
-            $this->APPLICATION_STORE[$post_code_hash][2] = $this->APPLICATION_STORE[$stringArray[0]][2] + $value;
-           
+            $this->APPLICATION_STORE[$post_code_hash][2] = $this->APPLICATION_STORE[$stringArray[0]][2] + $value; 
             return $this->APPLICATION_STORE;
         }
 
-        
         else{
             // var_dump($APPLICATION_STORE,'dewwd');
             $this->applicationParseAndStore($applicationString);
         }
-
-        
-
-
     }
-
-
-
 }
